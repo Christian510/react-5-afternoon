ec53faa4b2099bd51f1ed0347defac68bbaa0599
Christian's Mac <macbook@Macbooks-MacBook-Pro.local>
2019-05-22 21:16:33 -0600

    completed up to step 9

--- src/ducks/reducer.js
+++ src/ducks/reducer.js
@@ -18,32 +18,43 @@ const initialState = {
     email: ''
 }
 
-const UPDATE_LOAN_TYPE = UPDATE_LOAN_TYPE;
-const UPDATE_PROPERTY_TYPE = UPDATE_PROPERTY_TYPE;
+const UPDATE_LOAN_TYPE = "UPDATE_LOAN_TYPE";
+const UPDATE_PROPERTY_TYPE = "UPDATE_PROPERTY_TYPE";
+const UPDATE_CITY = "UPDATE_CITY";
 
-function reducer(state=initialState, action) {
-    switch(action.type){
+function reducer(state = initialState, action) {
+    switch (action.type) {
         case UPDATE_LOAN_TYPE:
-        return Object.assign( {}, state, {loanType: action.payload});
+            return Object.assign({}, state, { loanType: action.payload });
         case UPDATE_PROPERTY_TYPE:
-        return Object.assign({}, state, {propertyType: action.payload});
+            return Object.assign({}, state, { propertyType: action.payload });
+        case UPDATE_CITY:
+            return Object.assign({}, state, {
+                city:
+                    action.payload
+            })
 
         default: return state;
     }
-
-    export function updateLoanType(loanType){
-        return {
-            type: UPDATE_LOAN_TYPE,
-            payload: loanType
-        }
+}
+export function updateLoanType(loanType) {
+    return {
+        type: UPDATE_LOAN_TYPE,
+        payload: loanType
     }
-    export function upodatePropertyType(property){
-        return{
-            type: UPDATE_PROPERTY_TYPE,
-            payload: property
-        }
+}
+export function updatePropertyType(property) {
+    return {
+        type: UPDATE_PROPERTY_TYPE,
+        payload: property
     }
+}
 
+export function updateCity(city) {
+    return {
+        type: UPDATE_CITY,
+        payload: city
+    }
 }
 
 export default reducer;
