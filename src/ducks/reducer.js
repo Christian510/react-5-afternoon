41e5109e482b13c90a477e8169830b69683cd1ad
Christian's Mac <macbook@Macbooks-MacBook-Pro.local>
2019-05-23 09:01:00 -0600

    completed step 9

--- src/ducks/reducer.js
+++ src/ducks/reducer.js
@@ -21,6 +21,7 @@ const initialState = {
 const UPDATE_LOAN_TYPE = "UPDATE_LOAN_TYPE";
 const UPDATE_PROPERTY_TYPE = "UPDATE_PROPERTY_TYPE";
 const UPDATE_CITY = "UPDATE_CITY";
+const UPDATE_PROP = "UPDATE_PROP";
 
 function reducer(state = initialState, action) {
     switch (action.type) {
@@ -29,10 +30,10 @@ function reducer(state = initialState, action) {
         case UPDATE_PROPERTY_TYPE:
             return Object.assign({}, state, { propertyType: action.payload });
         case UPDATE_CITY:
-            return Object.assign({}, state, {
-                city:
-                    action.payload
-            })
+            return Object.assign({}, state, {city: action.payload});
+        case UPDATE_PROP:
+            return Object.assign({}, state, {updateProp: action.payload});
+        
 
         default: return state;
     }
@@ -54,6 +55,13 @@ export function updateCity(city) {
     return {
         type: UPDATE_CITY,
         payload: city
+    }
+}
+
+export function updateProp(prop) {
+    return {
+        type: UPDATE_PROP,
+        payload: prop
     }
 }
 
