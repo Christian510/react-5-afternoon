f9a9463d76f2780cbde8e370b3ad408d89da51c7
Christian's Mac <macbook@Macbooks-MacBook-Pro.local>
2019-05-23 10:10:54 -0600

    completed step 11

--- src/ducks/reducer.js
+++ src/ducks/reducer.js
@@ -23,6 +23,16 @@ const UPDATE_PROPERTY_TYPE = "UPDATE_PROPERTY_TYPE";
 const UPDATE_CITY = "UPDATE_CITY";
 const UPDATE_PROP = "UPDATE_PROP";
 const UPDATE_FOUND = "UPDATE_FOUND";
+const UPDATE_COST = "UPDATE_COST";
+const UPDATE_DOWNPAYMENT = "UPDATE_DOWNPAYMENT";
+const UPDATE_CREDIT = "UPDATE_CREDIT";
+const UPDATE_HISTORY = "UPDATE_HISTORY";
+const UPDATE_ADDRESS_ONE = "UPDATE_ADDRESS_ONE";
+const UPDATE_ADDRESS_TWO = "UPDATE_ADDRESS_TWO";
+const UPDATE_ADDRESS_THREE = "UPDATE_ADDRESS_THREE";
+const UPDATE_FIRST_NAME = "UPDATE_FIRST_NAME";
+const UPDATE_LAST_NAME = "UPDATE_LAST_NAME";
+const UPDATE_EMAIL = "EMAIL";
 
 function reducer(state = initialState, action) {
     switch (action.type) {
@@ -31,11 +41,33 @@ function reducer(state = initialState, action) {
         case UPDATE_PROPERTY_TYPE:
             return Object.assign({}, state, { propertyType: action.payload });
         case UPDATE_CITY:
-            return Object.assign({}, state, {city: action.payload});
+            return Object.assign({}, state, { city: action.payload });
         case UPDATE_PROP:
-            return Object.assign({}, state, {updateProp: action.payload});
+            return Object.assign({}, state, { updateProp: action.payload });
         case UPDATE_FOUND:
-            return Object.assign({}, state, {updateFound: action.payload});
+            return Object.assign({}, state, { updateFound: action.payload });
+        case UPDATE_REAL_ESTATE_AGENT:
+            return Object.assign({}, state, { updateRealStateAgent: action.payload });
+        case UPDATE_COST:
+            return Object.assign({}, state, { updateCost: action.payload });
+        case UPDATE_DOWNPAYMENT:
+            return Object.assign({}, state, { updatePayment: action.payload });
+        case UPDATE_CREDIT:
+            return Object.assign({}, state, {updateCredit : action.payload });
+        case UPDATE_HISTORY:
+            return Object.assign({}, state, { updateHistory: action.payload});
+        case UPDATE_ADDRESS_ONE:
+            return Object.assign({}, state, { updateAddressOne: action.payload});
+        case UPDATE_ADDRESS_TWO:
+            return Object.assign({}, state, { updateAddressTwo: action.payload});
+        case UPDATE_ADDRESS_THREE:
+            return Object.assign({}, state, { updateAddressThree: action.payload});
+        case UPDATE_FIRST_NAME:
+            return Object.assign({}, state, { updateFirstName: action.payload});
+        case UPDATE_LAST_NAME:
+            return Object.assign({}, state, { updateLastName: action.payload});
+        case UPDATE_EMAIL:
+            return Object.assign({}, state, { updateEmail: action.payload});
 
         default: return state;
     }
@@ -66,12 +98,81 @@ export function updateProp(prop) {
         payload: prop,
     }
 }
-export function updateFound( found ) {
+export function updateFound(found) {
     return {
         type: UPDATE_FOUND,
         payload: found
     }
 }
 
+export function updateRealStateAgent(agent) {
+    return {
+        type: UPDATE_REAL_ESTATE_AGENT,
+        payload: agent
+    }
+}
+
+export function updateCost ( cost ) {
+    return {
+        type: UPDATE_COST,
+            payload: cost
+    }
+}
+export function UPDATE_DOWNPAYMENT ( downPayment ) {
+    return {
+        type: UPDATE_DOWNPAYMENT,
+            payload: downPayment
+    }
+}
+export function UPDATE_CREDIT ( credit ) {
+    return {
+        type: UPDATE_CREDIT,
+            payload: credit
+    }
+}
+export function UPDATE_HISTORY ( history ) {
+    return {
+        type: UPDATE_HISTORY,
+            payload: history
+    }
+}
+export function UPDATE_ADDRESS_ONE ( addressOne ) {
+    return {
+        type: UPDATE_ADDRESS_ONE,
+            payload: addressOne
+    }
+}
+export function UPDATE_ADDRESS_TWO (addressTwo) {
+    return {
+        type: UPDATE_ADDRESS_TWO,
+            payload: addressTwo
+    }
+}
+export function UPDATE_ADDRESS_THREE ( addressThree ) {
+    return {
+        type: UPDATE_ADDRESS_THREE,
+            payload: addressThree
+    }
+}
+export function UPDATE_FIRST_NAME ( firstName ) {
+    return {
+        type: UPDATE_FIRST_NAME,
+            payload: firstName
+    }
+}
+
+export function UPDATE_LAST_NAME ( lastName ) {
+    return {
+        type: UPDATE_LAST_NAME,
+            payload: lastName
+    }
+}
+
+export function UPDATE_EMAIL ( email ) {
+    return {
+        type: UPDATE_EMAIL,
+            payload: email
+    }
+}
 
 export default reducer;
